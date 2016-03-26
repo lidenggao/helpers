f051a0da6e759a56c60ffc5918d26fc25430bc27
colee <5969226@qq.com>
2016-03-26 16:43:26 +0800

    init

---
+++ Format.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * ==============================================
+ * Copy right 2015-2016
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ * @param unknowtype
+ * @return return_type
+ * @author: CoLee
+ */
+
+namespace colee\helpers;
+
+class Format
+{
+    // 截字
+    public static function substr($string, $number)
+    {
+        $string = preg_replace('/\s/', '', strip_tags($string));
+        return mb_substr($string, 0, $number);
+    }
+}
