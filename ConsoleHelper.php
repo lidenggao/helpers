22085d0dbee438c0a0e86f81fb94419d41247309
colee <5969226@qq.com>
2016-07-21 18:33:17 +0800

    update

--- ConsoleHelper.php
+++ ConsoleHelper.php
@@ -4,6 +4,7 @@
  */
 namespace colee\helpers;
 
+use yii\helpers\Console;
 class ConsoleHelper
 {
     /**
@@ -24,12 +25,35 @@ class ConsoleHelper
     }
     
     /**
-     * @param  [type]
-     * @param  array
+     * @param string $string
+     * @param array $params
+     * @param string $color
+     *   text      text            background
+     *   ------------------------------------------------
+     *   %k %K %0    black     dark grey       black
+     *   %r %R %1    red       bold red        red
+     *   %g %G %2    green     bold green      green
+     *   %y %Y %3    yellow    bold yellow     yellow
+     *   %b %B %4    blue      bold blue       blue
+     *   %m %M %5    magenta   bold magenta    magenta
+     *   %p %P       magenta (think: purple)
+     *   %c %C %6    cyan      bold cyan       cyan
+     *   %w %W %7    white     bold white      white
+     *   %F     Blinking, Flashing
+     *   %U     Underline
+     *   %8     Reverse
+     *   %_,%9  Bold
+     *   %n     Resets the color
+     *   %%     A single %
      * @return [type]
      */
-    public static function log($string, $params=[])
+    public static function log($string, $params=[], $color=false)
     {
-        echo self::format($string, $params).PHP_EOL;
+        $string = self::format($string, $params).PHP_EOL;
+        if ($color) {
+            $string = Console::renderColoredString('%'.$color.$string);
+        }
+        
+        echo $string;
     }
 }
